57dc6d68a9ba5b933117c0688cabe9c8fd698fd3
Matheus Morcinek <matheusmorcinek@gmail.com>
2021-02-12 14:35:45 -0300

    first task done

--- bin2dec-app/src/app/app.component.ts
+++ bin2dec-app/src/app/app.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+
   title = 'my-app';
+
+  isValid: boolean = false;
+  result: number = 0;
+
+  binaryNumberInput = new FormControl('', {
+    validators: [Validators.pattern('^[0-1]*$')]
+  });
+
+  ngOnInit(): void {
+
+
+  }
+
+  validate() {
+
+    console.log(this.binaryNumberInput.valid);
+    this.binaryNumberInput.markAsTouched();
+  }
+
+  calculate(event: KeyboardEvent) {
+
+    if (this.binaryNumberInput.valid) {
+
+      let value = (event.target as HTMLInputElement).value;
+      console.log(value);
+    }else{
+
+      // remove result
+    }
+  }
+
 }
--- bin2dec-app/src/app/app.module.ts
+++ bin2dec-app/src/app/app.module.ts
@@ -4,6 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatCardModule} from '@angular/material/card';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -12,7 +18,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
